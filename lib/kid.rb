f30d7fffdb41794aea1aa5a578f05e95964460f0
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-23 13:13:07 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
-require_relative './dance_module.rb'
+require_relative './dance_module.rb' #includes the capabilities of the dance module
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance #accesses all of the methods of the dance module
+  extend MetaDancing
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
